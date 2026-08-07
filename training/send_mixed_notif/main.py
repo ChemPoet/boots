6c1fa96c4ce2bbeafd508fa0a4419512ebed1903
ChemPoet <dr.wynandj.louw@gmail.com>
2026-08-07 08:30:27 +0200

    send_mixed_notifications training start

---
+++ training/send_mixed_notif/main.py
@@ -0,0 +1,26 @@
+class EmailNotification:
+    def __init__(self, address, subject):
+        pass
+
+    def send(self):
+        pass
+
+
+class SMSNotification:
+    def __init__(self, phone_number, message):
+        pass
+
+    def send(self):
+        pass
+
+
+class PushNotification:
+    def __init__(self, username, message):
+        pass
+
+    def send(self):
+        pass
+
+
+def send_notifications(notifications):
+    pass
