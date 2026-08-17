37b169dc2c99bebaafc7d0b0bdb12aed9ce8222c
ChemPoet <dr.wynandj.louw@gmail.com>
2026-08-17 10:51:56 +0200

    asteroids Ch3L3-5

--- asteroids/asteroids/main.py
+++ asteroids/asteroids/main.py
@@ -1,8 +1,8 @@
 import pygame
 
-from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -14,8 +14,15 @@ def main():
     clock = pygame.time.Clock()
     # FPS tracked using float value
     dt = 0.0
-    ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # You can iterate over objects in a group
+        
+    Player.containers = (updatable, drawable)
+    # Player is the name of the class, not an instance of it
+    # All future instances of Player are in the Groups (updatable) and (drawable)
+    ship = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     # Game Loop
     while True:
@@ -23,9 +30,22 @@ def main():
     
         for event in pygame.event.get():
             pass
+        
+        for obj in updatable:
+            obj.update(dt)
+        # You may call the .update() method for every member of a group by calling it on the group itself
+        
             
+        # ship.update(dt)
+        # Update player movement before rendering
+        
         screen.fill("black")
-        ship.draw(screen)
+        for fig in drawable:
+            fig.draw(screen)
+        # Call the .draw() method for every member of the (drawable) group by calling it on the group itself
+                
+        # ship.draw(screen)
+        
         pygame.display.flip()
         
         for event in pygame.event.get():
